a034828ea397d47f0381410352117e48281dbe3b
zhengtianl <458078290@qq.com>
2023-04-26 23:55:39 +1000

    change

---
+++ src/FrontEnd/src/pages/CoverPages/index.js
@@ -0,0 +1,14 @@
+import { Card, Button } from 'antd'
+import './index.scss'
+
+const CoverPages = () => {
+  return (
+    <div className="login">
+        <Button className='but' type="primary" htmlType="submit" size="large" block>
+              Click open the world
+        </Button>
+    </div>
+  )
+}
+
+export default CoverPages
