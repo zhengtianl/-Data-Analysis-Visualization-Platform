a0611d7c32d35e83061ef5167b8f4c99f767474d
zhengtianl <starkfkworld@Zhengtians-MacBook-Pro.local>
2023-04-27 17:02:40 +1000

    change

--- src/FrontEnd/src/pages/CoverPages/index.js
+++ src/FrontEnd/src/pages/CoverPages/index.js
@@ -1,14 +1,26 @@
 import { Card, Button } from 'antd'
 import './index.scss'
+import { useState, useEffect } from 'react';
 
 const CoverPages = () => {
+
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="login">
-        <Button className='but' type="primary" htmlType="submit" size="large" block>
-              Click open the world
-        </Button>
+      <Button className='but'htmlType="submit" size="large" block>
+        Click open the world
+      </Button>
+      <Card className='time' >{time}</Card>
     </div>
   )
 }
 
-export default CoverPages
+export default CoverPages;
