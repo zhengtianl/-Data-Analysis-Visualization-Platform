2142d643feb6740f029414d49e2e6dc681f0fe7d
zhengtianl <458078290@qq.com>
2023-05-23 02:55:38 +1000

    change

--- src/renamesh.sh
+++ src/renamesh.sh
@@ -1,30 +1,17 @@
-#!/bin/bash
-
-OLD_EMAIL='starkfkworld@Zhengtians-MBP.hub'
-CORRECT_NAME='zhengtianl'
-CORRECT_EMAIL='458078290@qq.com'
-
 git filter-branch -f --env-filter "
-if [ \"\$GIT_COMMITTER_EMAIL\" = \"\$OLD_EMAIL\" ]
+
+OLD_EMAIL='starkfkworld@ravpn-266-1-student-10-8-43-73.uniaccess.unimelb.edu.au'
+CORRECT_NAME='zhengtianl'
+CORRECT_EMAIL='starkfkworld@ravpn-266-1-student-10-8-43-73.uniaccess.unimelb.edu.au'
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    if [ -z \"\$GIT_COMMITTER_NAME\" ]
-    then
-        export GIT_COMMITTER_NAME=\"\$CORRECT_NAME\"
-    fi
-    if [ -z \"\$GIT_COMMITTER_EMAIL\" ]
-    then
-        export GIT_COMMITTER_EMAIL=\"\$CORRECT_EMAIL\"
-    fi
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ \"\$GIT_AUTHOR_EMAIL\" = \"\$OLD_EMAIL\" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    if [ -z \"\$GIT_AUTHOR_NAME\" ]
-    then
-        export GIT_AUTHOR_NAME=\"\$CORRECT_NAME\"
-    fi
-    if [ -z \"\$GIT_AUTHOR_EMAIL\" ]
-    then
-        export GIT_AUTHOR_EMAIL=\"\$CORRECT_EMAIL\"
-    fi
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 " --tag-name-filter cat -- --branches --tags
